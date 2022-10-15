33d9d01f57406982a7d25cc1fa4c8ccd5f936e99
Bernardo <bevgim@gmail.com>
2022-10-15 01:14:16 -0300

    update 8

--- modulo_frontend/Pages/Calendario/javascript.js
+++ modulo_frontend/Pages/Calendario/javascript.js
@@ -32,7 +32,6 @@ const formatDate = date => {
 const postCateg = categories => {
     for(let i = 0; i < categories.length; i++){
         if(categories[i].includes('!') || categories[i] === 'Outros'){
-            console.log(categories[i])
             categories.splice(i--, 1);
         }
     }
